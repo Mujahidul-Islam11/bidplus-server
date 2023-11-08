f36ff0320cdb4ac5c50be0371217d3a7846f25f4
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-11-07 22:03:48 -0800

    I have created a new collection in the database and have posted data of the bider's in it

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-app.get('/', async(req, res)=>{
-    res.send('Skill swap server is running ')
-})
+app.get("/", async (req, res) => {
+  res.send("Skill swap server is running ");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8ojnwq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,50 +28,80 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const JobsData = client.db("SkillSwap").collection("JobsData");
+    const BidData = client.db("SkillSwap").collection("BidData");
 
-    app.post('/Jobs', async(req, res)=>{
-        const cursor = req.body;
-        const result = await JobsData.insertOne(cursor)
-        res.send(result)
+    app.post("/Jobs", async (req, res) => {
+      const cursor = req.body;
+      const result = await JobsData.insertOne(cursor);
+      res.send(result);
+    });
+
+    app.post("/Bids", async(req, res)=>{
+      const cursor = req.body;
+      const result = await BidData.insertOne(cursor)
+      res.send(result)
+    })
+    app.get("/Bids", async(req, res)=>{
+      const result = await BidData.find().toArray();
+      res.send(result)
     })
 
-    app.get('/Jobs', async(req, res)=>{
-        const result = await JobsData.find().toArray()
-        res.send(result)
-    })
+    app.get("/Jobs", async (req, res) => {
+      const result = await JobsData.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/Jobs/:category', async(req, res)=>{
-        const category = req.params.category
-        const query = { category: category }
-        const result = await JobsData.find(query).toArray()
-        res.send(result)
-    })
+    app.get("/Jobs/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const result = await JobsData.find(query).toArray();
+      res.send(result);
+    });
 
-    app.get('/JobsId/:id', async(req, res)=>{
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-        const result = await JobsData.findOne(query)
-        res.send(result)
-    })
-    app.put('/JobsId/:id', async (req, res) => {
-        const id = req.params.id
-        const filter = {_id: new ObjectId(id) }
-        const update =req.body
-        const cart = {
-          $set: {
-            name:update.email,
-            brandName:update.jobTitle,
-            rating:update.deadline,
-            price:update.price,
-            description:update.description,
-            type:update.category,
-          }
+    app.get("/JobsId/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await JobsData.findOne(query);
+      res.send(result);
+    });
+    app.get("/JobEmail", async (req, res) => {
+      let query = {}
+        if(req.query.email){
+            query={email :req.query.email}
         }
-        const result = await JobsData.updateOne(filter, cart);
-        res.send(result);
-      })
+      const result = await JobsData.find(query).toArray()
+      res.send(result)
+    });
+
+    app.put("/JobsId/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const update = req.body;
+      const cart = {
+        $set: {
+          name: update.email,
+          brandName: update.jobTitle,
+          rating: update.deadline,
+          price: update.price,
+          description: update.description,
+          type: update.category,
+        },
+      };
+      const result = await JobsData.updateOne(filter, cart);
+      res.send(result);
+    });
+
+    app.delete("/JobsId/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await JobsData.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -80,9 +109,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 app.listen(port, () => {
-    console.log(`My server is running on port: ${port}`);
-  });
+  console.log(`My server is running on port: ${port}`);
+});
